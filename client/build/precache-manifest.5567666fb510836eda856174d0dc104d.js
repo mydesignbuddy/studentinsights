c1e1e9dd76deb1f66d7f2e64d9d2db87d0a99ac5
Buddy Toups <mydesignbuddy@gmail.com>
2019-04-15 21:12:53 -0500

    build client for deploy

---
+++ client/build/precache-manifest.5567666fb510836eda856174d0dc104d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8938c4735cb1b83c2d90",
+    "url": "/static/js/main.1ae67de4.chunk.js"
+  },
+  {
+    "revision": "7ff3f49b30f3f15fb493",
+    "url": "/static/js/2.9acd57ae.chunk.js"
+  },
+  {
+    "revision": "8938c4735cb1b83c2d90",
+    "url": "/static/css/main.fc011c67.chunk.css"
+  },
+  {
+    "revision": "34eb49ae13ce8699dd52e1ff26523abf",
+    "url": "/index.html"
+  }
+];
